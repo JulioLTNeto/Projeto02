a903ffedeaebdce54494c92329d7d62ca38d57db
Ezequiel Teotônio Jó <41303804+ezequieltjo@users.noreply.github.com>
2018-09-17 12:37:15 -0600

    Add files via upload

--- Projeto02/src/Terreno.cpp
+++ Projeto02/src/Terreno.cpp
@@ -2,7 +2,25 @@
 
 Terreno::Terreno()
 {
-    //ctor
+    area = 0;
+}
+void Terreno::setArea(double area){
+    this->area = area;
+}
+
+void Terreno::setDescricao(){
+    std::stringstream sArea;
+    sArea << area;
+
+    descricao = posicao + "/" + sArea.str();
+}
+
+double Terreno::getArea(){
+    return area;
+}
+
+std::string Terreno::getDescricao(){
+    return descricao;
 }
 
 Terreno::~Terreno()
