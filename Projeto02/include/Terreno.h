40d41ec84172700e12045ae2c81759688534a219
Ezequiel Teotônio Jó <41303804+ezequieltjo@users.noreply.github.com>
2018-09-17 12:36:39 -0600

    Add files via upload

--- Projeto02/include/Terreno.h
+++ Projeto02/include/Terreno.h
@@ -1,16 +1,26 @@
 #ifndef TERRENO_H
 #define TERRENO_H
 
+#include "Imovel.h"
+#include <string>
+#include <sstream>
 
-class Terreno
+class Terreno : public Imovel
 {
     public:
         Terreno();
         virtual ~Terreno();
 
+        void setArea(double area);
+        void setDescricao();
+
+        double getArea();
+        std::string getDescricao();
+
     protected:
 
     private:
+        double area;
 };
 
 #endif // TERRENO_H
